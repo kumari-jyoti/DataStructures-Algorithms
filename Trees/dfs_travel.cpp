0bafde6b0c4b5a97d086be51f38717d21da8e7fe
kumari-jyoti <mejyotikumari31@gmail.com>
2016-10-13 18:38:20 +0530

    Commented

--- Trees/dfs_travel.cpp
+++ Trees/dfs_travel.cpp
@@ -1,12 +1,24 @@
+/*Basic Tree Operations
+1. Inorder Traversal
+2. Preorder Traversal
+3. Postorder Traversal
+4. Height of a Tree
+5. Size of a Tree
+6. Searching a key
+7. Inserting a key
+*/
+
 #include<iostream>
 using namespace std;
 
+//defining the tree node
 struct node{
 	int data;
 	node* left;
 	node* right;
 };
 
+//creating new tree node
 node* newNode(int val){
 	struct node* pnode=new node;
 	pnode->data=val;
@@ -15,54 +27,62 @@ node* newNode(int val){
 	return pnode;
 }
 
+//recursive inorder traversal
 void inorder(node* root){
 	if(!root) return;
-	inorder(root->left);
-	cout<<root->data<<" ";
-	inorder(root->right);
+	inorder(root->left);//traverse left
+	cout<<root->data<<" ";//traverse root
+	inorder(root->right);//traverse right
 }
+
+//recursive pre-order traversal
 void preorder(node* root){
 	if(!root) return;
-	cout<<root->data<<" ";
-	preorder(root->left);	
-	preorder(root->right);
+	cout<<root->data<<" ";//traverse root
+	preorder(root->left);//traverse left	
+	preorder(root->right);//traverse right
 }
+
+//recursive postorder traversal
 void postorder(node* root){
 	if(!root) return;
-	postorder(root->left);	
-	postorder(root->right);
-	cout<<root->data<<" ";
+	postorder(root->left);//traverse left	
+	postorder(root->right);//traverse right
+	cout<<root->data<<" ";//traverse root
 }
 
 int max(int a, int b){return (a>b?a:b);
 }
 
+//finding height of a tree
 int height(node* root){
 	if(!root) return 0;
 	return max(1+height(root->left),1+height(root->right));
 }
 
+//finding size of a tree
 int countofnodes(node* root){
 	if(!root) return 0;
 	return (1+countofnodes(root->left)+countofnodes(root->right));
 }
 
+//searching a key
 void search(node* root, int key){
 	if(!root){
 		cout<<"not there"<<endl;
 		return;
 	}
-	if(root->data==key){
+	if(root->data==key){		
 		cout<<"found"<<endl;
 		return;
 	}
 	if(key>root->data)
 		return search(root->right,key);
 	else
-		return search(root->left,key);
-	
+		return search(root->left,key);	
 }
 
+//inserting key in the tree
 void insert(node* root, int key)
 {
 	node* pnode=newNode(key);
@@ -87,6 +107,7 @@ void insert(node* root, int key)
 	return;
 }
 
+//main function
 int main(){
 	node* root=newNode(20);
 	root->left=newNode(13);
